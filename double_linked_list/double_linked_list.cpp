9a4527feb2aacd39e4d78f8276c1e656ca18a9d3
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-05-25 11:33:53 +0700

    membuat perulangan dan mencari node

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -59,22 +59,26 @@ void DoubleLinkedList::addnote() {
 	while (current->next != NULL && nim == current->next->noMhs) {
 		current = current->next;
 
-	if (current->next != NULL && nim == current->next->noMhs) {
-		cout << "\nDuplicate roll numbers not allowed" << endl;
-		return;
-	}
+		if (current->next != NULL && nim == current->next->noMhs) {
+			cout << "\nDuplicate roll numbers not allowed" << endl;
+			return;
+		}
 
-	newNode->next = current->next;
-	newNode->prev = current;
-	if (current->next != NULL)
-		current->next->prev = newNode;
-	current->next = newNode;
+		newNode->next = current->next;
+		newNode->prev = current;
+		if (current->next != NULL)
+		{
+			current->next->prev = newNode;
+			current->next = newNode;
+		}
+	}
 }
 
-
-
-
-
-	
-
+bool DoubleLinkedList::search(int rollNo, Node * *previous, Node * *current) {
+	*previous = *current = START;
+	while (*current != NULL && rollNo != (*current)->noMhs) {
+		*previous = *current;
+		*current = (*current)->next;
+	}
+	return (*current != NULL);
 }
