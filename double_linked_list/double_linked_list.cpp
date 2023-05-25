2a81bc0b5345674614596178d75ef08d649813c1
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-05-25 15:26:34 +0700

    memasukan menu switch

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -178,6 +178,26 @@ int main() {
 			char ch;
 			cin >> ch;
 
+			switch (ch) {
+			case '1':
+				obj.addnote();
+				break;
+			case '2':
+				obj.hapus();
+				break;
+			case '3':
+				obj.ascending();
+				break;
+			case '4':
+				obj.descending();
+				break;
+			case '5':
+				obj.searchData();
+				break;
+			case '6':
+				return 0;
+			}
+
 		}
 	}
 }
