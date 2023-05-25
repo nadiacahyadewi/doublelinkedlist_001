617b9f37f5acef0a96c92faf5fbe4ff220df31cd
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-05-25 11:00:45 +0700

    memulai program dan membuat program newNode

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -42,6 +42,19 @@ void DoubleLinkedList::addnote() {
 	newNode->noMhs = nim;
 	newNode->name = nm;
 
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START;
+		if (START != NULL)
+			START->prev = newNode;
+		newNode->prev = NULL;
+		START = newNode;
+		return;
+	}
+
 	
 
 }
